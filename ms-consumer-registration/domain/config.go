183fb84c3441e67cfb1c4da929bad03cbf7cdfa1
Adam Syarif Hidayatullah <adamsyarif219@gmail.com>
2021-07-03 06:55:01 +0700

    add redis client

--- ms-consumer-registration/domain/config.go
+++ ms-consumer-registration/domain/config.go
@@ -1,9 +1,14 @@
 package domain
 
-import "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	"context"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/go-redis/redis/v8"
+)
 
 type Config struct {
 	KafkaConsumer *kafka.Consumer
+	RedisClient *redis.Client
 }
 
 func LoadConfig() (config Config, err error){
@@ -21,6 +26,17 @@ func LoadConfig() (config Config, err error){
 	}
 	config.KafkaConsumer = consumer
 
+	// redis
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6377",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	ping := redisClient.Ping(context.Background())
+	if ping.Err() != nil {
+		return config, ping.Err()
+	}
+	config.RedisClient = redisClient
 
 	return config, err
 }
--- ms-consumer-registration/go.mod
+++ ms-consumer-registration/go.mod
@@ -2,4 +2,7 @@ module ms-consumer-registration
 
 go 1.16
 
-require github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go v1.7.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.0 // indirect
+)
